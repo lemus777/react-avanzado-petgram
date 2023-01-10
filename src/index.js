9deba0904f4aa2ede11ba1929a5e40537fd2bbe1
lemus <lemus777@gmail.com>
2023-01-10 13:05:17 +0100

    agregadas dependencias

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(<h1>Seguimos con el curso avanzado!</h1>);
