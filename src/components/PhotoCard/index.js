bf1c84f06224663df9944c8b39759fe9a024fca4
lemus <lemus777@gmail.com>
2023-01-17 23:32:35 +0100

    custom hooks para likes en localStorage y useNearScreen

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,27 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorate } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen() // la constante es lo que retorna el hook useNearScreen
+  const key = `like-${id}` // esta key incluye la id para ser única para cada elemento
+  const [liked, setLiked] = useLocalStorate(key, false) // usaremos el localStorage con la key declarada, de valor inicial false (se usará si no hay localStorage guardada, ver la lógica de la función en el customHook)
 
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
+  console.log(liked)
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -33,8 +24,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size='32px' />{likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' />{likes} likes!
           </Button>
         </>
       }
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export function useLocalStorate (key, initialValue) {
+  const [storedValue, setValue] = useState(() => { // esta funcion declara el estado inicial
+    try {
+      const item = window.localStorage.getItem(key) // intenta obtener key del localStorage y guardarla como item
+      return item !== null ? JSON.parse(item) : initialValue // si hay algo guardado item no es null y lo parsea (porque será una string). Si no existe retorna el initialValue
+    } catch (e) {
+      return initialValue // si da algun error también retorna el inicialValue (recordar que está declarado como un parámetro de la función)
+    }
+  })
+
+  const setLocalStorage = value => { // para actualizar el localStorage
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value)) // le damos de parámetros key y el string de value
+      setValue(value) // va a actualizar el valor con value
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  return [storedValue, setLocalStorage] // necesita retornar dos cosas: el valor guardado y un actualizador del valor
+}
