5732143cd4d2659d611fb6f57e82098fd056a9ce
lemus <lemus777@gmail.com>
2023-01-18 23:15:57 +0100

    cargando fotografias reales

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorate } from '../../hooks/useLocalStorage'
--- src/index.js
+++ src/index.js
@@ -6,3 +6,25 @@ const container = document.getElementById('app')
 const root = createRoot(container)
 
 root.render(<App />)
+
+// Así sería si funcionara vercel
+// DEPENDENCIES
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// APP
+// import App from "./App";
+
+// const client = new ApolloClient({
+//  uri: "https://petgram-server-clgg.vercel.app/graphql",
+//  cache: new InMemoryCache(),
+// });
+
+// ReactDOM.render(
+//  <ApolloProvider client={client}>
+//    <App />
+//  </ApolloProvider>,
+
+//  document.getElementById("App")
+// );
