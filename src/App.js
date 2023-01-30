d766fd74c3ebbd16542e0e82f9409fb5f725b3f9
lemus <lemus777@gmail.com>
2023-01-30 22:51:03 +0100

    rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
+
 import { Detail } from './pages/Detail'
 import { Home } from './pages/Home'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 export const App = () => {
   return (
     <div>
@@ -17,6 +25,20 @@ export const App = () => {
           <Route path='/pet/:id' element={<Home />} />
           <Route path='/detail/:detailId' element={<Detail />} />
         </Routes>
+        <UserLogged>
+          {
+            ({ isAuth }) =>
+              isAuth
+                ? <Routes>
+                  <Route path='/favs' element={<Favs />} />
+                  <Route path='/user' element={<User />} />
+                </Routes>
+                : <Routes>
+                  <Route path='/favs' element={<NotRegisteredUser />} />
+                  <Route path='/user' element={<NotRegisteredUser />} />
+                </Routes>
+          }
+        </UserLogged>
         <NavBar />
       </BrowserRouter>
     </div>
